c0261704934f5d7201073cb3c2ff043a6eaf56b1
geunheejung <geunhee0212@gmail.com>
2023-08-18 19:51:58 +0900

    add github user searc form.

--- App.tsx
+++ App.tsx
@@ -5,24 +5,28 @@
  * @format
  */
 
-import React from 'react';
+import React, {useCallback} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Alert,
+  Button,
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
+import axios from 'axios';
+import {GITHUB_TOKEN} from '@env';
 
 import {
   Colors,
-  DebugInstructions,
   Header,
   LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
@@ -55,10 +59,80 @@ function Section({children, title}: SectionProps): JSX.Element {
   );
 }
 
-const HelloWorld = () => {
+const customAxios = axios.create({
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+  },
+});
+
+interface IUserInfo {
+  name: string;
+  avatar_url: string;
+}
+
+const getGithubUser = async (username: string) => {
+  try {
+    // https://api.github.com/users/
+    const URL = 'https://api.github.com/users/';
+
+    const res = await customAxios.get<IUserInfo>(`${URL}${username}`);
+
+    return res.data;
+  } catch (error: any) {
+    console.error(error.message);
+  }
+};
+
+const UserSearchForm = () => {
+  const [username, setUserName] = React.useState('');
+  const [userInfo, setUserInfo] = React.useState<IUserInfo>();
+
+  const getUser = async (username: string) => {
+    const user = await getGithubUser(username);
+
+    if (!user) {
+      Alert.alert('사용자를 찾을 수 없습니다.');
+      return;
+    }
+
+    setUserInfo(user);
+    setUserName('');
+  };
+
+  const handleChangeText = useCallback((text: string) => {
+    setUserName(text);
+  }, []);
+
+  const handlePress = useCallback(() => {
+    if (!username) {
+      Alert.alert('유저 이름을 입력하세요');
+      return;
+    }
+    getUser(username);
+  }, [username]);
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Try editing me!</Text>
+    <View style={styles.searchForm}>
+      {userInfo && (
+        <View>
+          <Image
+            source={{
+              uri: userInfo.avatar_url,
+            }}
+            style={{width: 150, height: 150}}
+          />
+          <Text>{userInfo.name}</Text>
+        </View>
+      )}
+      <TextInput
+        style={styles.input}
+        placeholder="사용자 이름을 검색하세요"
+        value={username}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={handleChangeText}
+      />
+      <Button onPress={handlePress} title="Search" />
     </View>
   );
 };
@@ -80,7 +154,7 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        <HelloWorld />
+        <UserSearchForm />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
@@ -112,6 +186,15 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  input: {
+    height: 50,
+    borderRadius: 5,
+    padding: 5,
+  },
+  searchForm: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
